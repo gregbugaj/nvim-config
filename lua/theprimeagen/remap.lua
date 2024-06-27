afe63fc948c47b3aa5a1ad5a8ad5043cbc500c1d
Greg <gregbugaj@yahoo.com>
2024-06-27 06:38:50 -0500

    wip: tweaking and re-mapping

--- lua/theprimeagen/remap.lua
+++ lua/theprimeagen/remap.lua
@@ -34,13 +34,6 @@ map('n', '<Leader>n', ':NvimTreeFocus<CR>', opts)
 -- vim.keymap.set("n", "N", "Nzzzv")
 -- vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 
--- vim.keymap.set("n", "<leader>vwm", function()
---     require("vim-with-me").StartVimWithMe()
--- end)
--- vim.keymap.set("n", "<leader>svwm", function()
---     require("vim-with-me").StopVimWithMe()
--- end)
-
 -- -- greatest remap ever
 -- vim.keymap.set("x", "<leader>p", [["_dP]])
 
@@ -55,12 +48,12 @@ map('n', '<Leader>n', ':NvimTreeFocus<CR>', opts)
 
 -- vim.keymap.set("n", "Q", "<nop>")
 -- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
--- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
--- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
--- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
--- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
