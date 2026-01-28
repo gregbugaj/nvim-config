8fad3a05989d797a4609b6e7821be45cd1ef6fbd
gregbugaj <gregbugaj@yahoo.com>
2026-01-28 06:49:38 -0600

    wip: config upates

--- lua/theprimeagen/lazy/local.lua
+++ lua/theprimeagen/lazy/local.lua
@@ -7,7 +7,7 @@ local local_plugins = {
             local harpoon = require("harpoon")
 
             harpoon:setup()
-
+   
             vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
             vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
